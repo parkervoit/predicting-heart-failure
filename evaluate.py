7e6c5b00d5377655197dccd29bb7f49ac295a6df
parkervoit <parker.r.voit@gmail.com>
2021-07-09 11:11:02 -0500

    added a few modules, workbook, and readme.md

---
+++ evaluate.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
+
+def run_metrics(X, y, model, data_set = 'This'):
+    """
+    Takes in X , target as y, the model for testing, and the data_set(i.e. train, validate, test)\n
+    Outputs a print list with the confusion matrix, classification report, confusion matrix, and the T/F +/- rate
+    """
+    score = model.score(X, y)
+    matrix = confusion_matrix(y, model.predict(X))
+    tpr = matrix[1,1] / (matrix[1,1] + matrix[1,0])
+    fpr = matrix[0,1] / (matrix[0,1] + matrix[0,0])
+    tnr = matrix[0,0] / (matrix[0,0] + matrix[0,1])
+    fnr = matrix[1,0] / (matrix[1,1] + matrix[1,0])
+    prc = matrix[1,1] / (matrix[1,1] + matrix[0,1])
+    
+    print(f'{data_set} data set accuracy score: {score:.2%}')
+    print(f'{data_set} data set precision score {prc:.2%}')
+    print(f'{data_set} data set recall score: {tpr:.2%}\n')
+    class_report = classification_report(y, model.predict(X), zero_division=True)
+    print('-------------------------------')
+    print(f'classification report')
+    print(class_report)
+    print ('-------------------------------\n')
+    print('confusion matrix')
+    print(f'{matrix}\n')
+    print(f'{data_set} data set model metrics')
+    print('---------------------------------')
+    print(f'True positive rate for the model is {tpr:.2%}')
+    print(f'False positive rate for the model is  {fpr:.2%}')
+    print(f'True negative rate for the model is {tnr:.2%}')
+    print(f'False negative rate for the model is {fnr:.2%}\n')
